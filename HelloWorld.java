5e815186fd06220dc892fdc06fa4d2723807a46a
AnishSonu <anishagrawal674@gmail.com>
2017-09-13 16:14:43 +0530

    added all files

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
-class A
-{
-    public static void main(String args[])
-  {
-     System.out.println("Hello world");
-  }
+public class HelloWorld
+ {
+    public static void main( String[] args ) 
+    {
+        System.out.println( "Hello World!" );
+        System.exit( 0 ); //success
+    }
 }
